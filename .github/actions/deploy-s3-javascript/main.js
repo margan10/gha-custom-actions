ee94ebbfd1fda778c002fe41807b22d95719460a
Shin <margan10@o2.pl>
2023-10-12 15:00:55 +0200

    finished js deploy but without actual deploy line

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -13,7 +13,7 @@ function run() {
     const s3Uri = `s3://${bucket}`;
     //exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --rgion ${bucketRegion}`); //this will look for environment variables to authenticate request
 
-    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonws.com}`;
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
     core.setOutput('website-url', websiteUrl); //equivalent to echo 
 
     core.notice(`Hello from my custom JavaScript Action! ${websiteUrl} `)
