d92f5ec26601d1b63f1f27ab21d366dfc88137a4
Shin <margan10@o2.pl>
2023-10-12 14:51:36 +0200

    finished js deploy but without actual deploy line

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,21 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
+//const github = require('@actions/github')
 const exec = require('@actions/exec')
 function run() {
+    // 1) Get some input values
+    const bucket = core.getInput('bucket', {required:true});
+    const bucketRegion = core.getInput('bucket-region', {required: true});
+    const distFolder = core.getInput('dist-folder', {required:true})
     
+    //github.getOctokit() we dont use it here, but could be used for accessing github REST API
+
+    //2) Upload files
+    const s3Uri = `s3://${bucket}`;
+    //exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --rgion ${bucketRegion}`); //this will look for environment variables to authenticate request
+
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonws.com}`;
+    core.setOutput('website- url', websiteUrl); //equivalent to echo 
+
     core.notice('Hello from my custom JavaScript Action!')
 }
 
